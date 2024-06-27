159da7d26ad63a3125858be93095689228f19ce2
alaaWalid <alaa2alshitany@gmail.com>
2024-06-28 00:13:25 +0300

    Added Jenkins Service Account

---
+++ Terraform/service-account.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_service_account" "jenkins-SA" {
+  metadata {
+    name = "jenkins-service-account"
+    namespace = var.ns-names[1]
+  }
+}
+
+# resource "kubernetes_secret" "jenkins-SA-secret" {
+#   metadata {
+#     annotations = {
+#       "kubernetes.io/service-account.name" = kubernetes_service_account.jenkins-SA.metadata.0.name
+#     }
+#     generate_name = "jenkins-SA-secret"
+#   }
+
+#   type                           = "kubernetes.io/service-account-token"
+#   wait_for_service_account_token = true
+# }
