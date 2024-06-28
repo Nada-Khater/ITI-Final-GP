cfe0933e604b6420f074e5f4d0444dc0b8c6507a
alaaWalid <alaa2alshitany@gmail.com>
2024-06-28 22:13:13 +0300

    modify role & binding

--- Terraform/role&role_binding.tf
+++ Terraform/role&role_binding.tf
@@ -1,9 +1,7 @@
-resource "kubernetes_role" "jenkins_role" {
+resource "kubernetes_cluster_role" "jenkins_role" {
   metadata {
     name      = "jenkins-role"
-    namespace = var.ns-names[1]
   }
-
   rule {
     api_groups = [""]
     resources  = ["pods", "pods/log", "services", "deployments", "configmaps", "secrets"]
@@ -11,21 +9,21 @@ resource "kubernetes_role" "jenkins_role" {
   }
 }
 
-resource "kubernetes_role_binding" "jenkins_role_binding" {
+resource "kubernetes_cluster_role_binding" "jenkins_role_binding" {
   metadata {
     name      = "jenkins_role_binding"
-    namespace = var.ns-names[1]
   }
 
   subject {
     kind      = "ServiceAccount"
-    name      = kubernetes_service_account.jenkins-SA.metadata[0].name
+    name      = kubernetes_cluster_role.jenkins_role.metadata[0].name
     namespace = var.ns-names[1]
   }
 
   role_ref {
-    kind     = "Role"
-    name     = kubernetes_role.jenkins_role.metadata[0].name
+    kind     = "ClusterRole"
+    name     = kubernetes_cluster_role.jenkins_role.metadata[0].name
     api_group = "rbac.authorization.k8s.io"
   }
 }
+
