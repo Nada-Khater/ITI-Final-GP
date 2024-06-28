0f56e2b1b3b002066c0583a89d079095dd21f14b
alaaWalid <alaa2alshitany@gmail.com>
2024-06-28 22:12:52 +0300

    make secret for jenkins serrvice account

--- Terraform/service-account.tf
+++ Terraform/service-account.tf
@@ -5,14 +5,14 @@ resource "kubernetes_service_account" "jenkins-SA" {
   }
 }
 
-# resource "kubernetes_secret" "jenkins-SA-secret" {
-#   metadata {
-#     annotations = {
-#       "kubernetes.io/service-account.name" = kubernetes_service_account.jenkins-SA.metadata.0.name
-#     }
-#     generate_name = "jenkins-SA-secret"
-#   }
+resource "kubernetes_secret" "jenkins-SA-secret" {
+  metadata {
+    annotations = {
+      "kubernetes.io/service-account.name" = kubernetes_service_account.jenkins-SA.metadata[0].name
+    }
+    generate_name = "jenkins-SA-secret"
+  }
 
-#   type                           = "kubernetes.io/service-account-token"
-#   wait_for_service_account_token = true
-# }
+  type                           = "kubernetes.io/service-account-token"
+  wait_for_service_account_token = true
+}
